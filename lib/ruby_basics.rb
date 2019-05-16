91853129098a9a31313708f2eca823006b073cb3
Zach Parker <parkerz410@gmail.com>
2019-05-16 15:48:35 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ num = num1 / num2
+ num
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
+  value
 end
 
-def argue
-  
-end
-
-def greeting
+def argue (argument)
 
 end
 
-def return_a_value
+def greeting (greeting, name)
 
 end
 
-def last_evaluated_value
+def return_a_value("Nice")
 
 end
 
-def pizza_party
+def last_evaluated_value ("expert")
+
+end
+
+def pizza_party("cheese")
 
 end
